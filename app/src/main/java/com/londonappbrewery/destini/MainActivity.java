3f0c717dc81a435267636dac6b6155ad88008c15
Vivek Gawande <vivek040997@gmail.com>
2019-05-15 14:06:20 +0530

    Add All paths and Reset App functionality

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,15 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView mTextView;
+    Button mAns1Button, mAns2Button;
+    int mStoryState = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,17 +18,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mTextView = findViewById(R.id.storyTextView);
+        mAns1Button = findViewById(R.id.buttonTop);
+        mAns2Button = findViewById(R.id.buttonBottom);
 
+        mAns1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStoryState == 1 || mStoryState == 2) {
+                    mTextView.setText(R.string.T3_Story);
+                    mAns1Button.setText(R.string.T3_Ans1);
+                    mAns2Button.setText(R.string.T3_Ans2);
+                    mStoryState = 3;
+                } else if (mStoryState == 3) {
+                    mTextView.setText(R.string.T6_End);
+                    mAns1Button.setText(R.string.reset);
+                    mAns2Button.setVisibility(View.INVISIBLE);
+                    mStoryState = 4;
+                } else if (mStoryState == 4) {
+                    // Reset the game
+                    mTextView.setText(R.string.T1_Story);
+                    mAns1Button.setText(R.string.T1_Ans1);
+                    mAns2Button.setText(R.string.T1_Ans2);
+                    mAns2Button.setVisibility(View.VISIBLE);
+                    mStoryState = 1;
+                }
+            }
+        });
 
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
-
+        mAns2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mStoryState == 1) {
+                    mTextView.setText(R.string.T2_Story);
+                    mAns1Button.setText(R.string.T2_Ans1);
+                    mAns2Button.setText(R.string.T2_Ans2);
+                    mStoryState = 2;
+                } else if (mStoryState == 2) {
+                    mTextView.setText(R.string.T4_End);
+                    mAns1Button.setText(R.string.reset);
+                    mAns2Button.setVisibility(View.INVISIBLE);
+                    mStoryState = 4;
+                } else if (mStoryState == 3) {
+                    mTextView.setText(R.string.T5_End);
+                    mAns1Button.setText(R.string.reset);
+                    mAns2Button.setVisibility(View.INVISIBLE);
+                    mStoryState = 4;
+                }
+            }
+        });
 
     }
 }
